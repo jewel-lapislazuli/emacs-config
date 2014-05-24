0f3f6757e42469de27467687a38edf99bca0aa07
Kazutoshi Nakagawa <jewel.lapislazuli@gmail.com>
2014-05-24 20:40:43 +0900

    web-modeの設定に色を追加、その他の設定を修正

--- inits/12_web-mode.el
+++ inits/12_web-mode.el
@@ -1,22 +1,30 @@
-; web-modeの設定
+;;; web-modeの設定
 (require 'web-mode)
 
-;;; 適用する拡張子
-(add-to-list 'auto-mode-alist '("\\.phtml$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsp$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?$" . web-mode))
+; web-modeを適用するファイルの設定
+(setq auto-mode-alist (append (list
+			       '("\\.phtml\\'" . web-mode)
+			       '("\\.php\\'" . web-mode)
+			       '("\\.jsp\\'" . web-mode)
+			       '("\\.as[cp]x\\'" . web-mode)
+			       '("\\.erb\\'" . web-mode)
+			       '("\\.html\\'" . web-mode)
+			       )
+			      auto-mode-alist))
 
-;;; インデント数
+; インデントの設定
 (defun web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-html-offset 2)
   (setq web-mode-css-offset 2)
-  (setq web-mode-script-offset 2)
-  (setq web-mode-php-offset 2)
-  (setq web-mode-java-offset 2)
+  (setq web-mode-script-offset 4)
+  (setq web-mode-php-offset 4)
+  (setq web-mode-java-offset 4)
   (setq web-mode-asp-offset 2))
 (add-hook 'web-mode-hook 'web-mode-hook)
 
+;; 色の設定
+; HTML
+(set-face-attribute 'web-mode-html-tag-face nil :foreground "yellow")
+(set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "white")
+(set-face-attribute 'web-mode-html-attr-name-face nil :foreground "CadetBlue2")
